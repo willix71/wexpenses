a14d3e47b17de3146c2d9cab6a54b6b56925cb5f
William Keyser <willy@wilson.(none)>
2013-07-13 16:12:50 +0200

    added findByPayee

--- core/src/main/java/w/wexpense/persistence/dao/IExpenseJpaDao.java
+++ core/src/main/java/w/wexpense/persistence/dao/IExpenseJpaDao.java
@@ -8,9 +8,12 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import w.wexpense.model.Expense;
+import w.wexpense.model.Payee;
 import w.wexpense.model.Payment;
 
 public interface IExpenseJpaDao extends JpaRepository< Expense, Long >, JpaSpecificationExecutor< Expense >, IDBableJpaDao<Expense> {
+	List<Expense> findByPayee(Payee p);
+
 	List<Expense> findByPayment(Payment p);
 	
 	@Query(value="from Expense x where x.payment=?1 and x.uid not in (?2)")
